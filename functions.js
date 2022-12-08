c1217d4f4d0b12785a21a88d70057a7725336073
Kingsley Mgbachi <email@example.com>
2022-12-08 18:50:24 +0100

    made changes to the function js

--- functions.js
+++ functions.js
@@ -1,3 +1,27 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pArray) => {
+  let firstElement = pArray[0];
+  let dictionary = pArray[1];
+
+  let str= dictionary.split(",");
+  let answerWords = "";
+  str.map((firstWord) => {
+    str.map((word) => {
+      let joinedWord = firstWord + word;
+      let reversedWord = [joinedWord].reverse().toString();
+
+      if (joinedWord == firstElement || reversedWord == firstElement) {
+        return (answerWords = firstWord + "," + word);
+      } else {
+        return "false";
+      }
+    });
+  });
+  
+  if (answerWords == "") {
+    return "false";
+  } else {
+    return answerWords;
+  }
+};
 
 export { isTheWordSplit };
